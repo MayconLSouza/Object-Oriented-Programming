7ec0c2ed22e9fc157d075ecfd38b1edc4b1ca978
MayconLSouza <mayconl.martins@outlook.com>
2024-05-04 15:19:48 -0300

    Adding exercise from class 11

---
+++ Class11_MySQL-Connection/src/view/Teste.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import persistence.GenericDao;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		GenericDao gDao = new GenericDao();
+		
+		try {
+			Connection c = gDao.getConnection();
+			String sgbd = c.getMetaData().getDatabaseProductName();
+			System.out.println(sgbd);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
